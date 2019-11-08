95ac6c5d8781026b9ee25e21d04aa04ca2100ed9
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-08 14:03:13 -0800

    Remove identity-controller dist folder
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- identity-controller/dist/core/database/database.model.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ava_1 = require("ava");
-const database_model_1 = require("./database.model");
-const prefix = 'VALIDATORS: ';
-ava_1.default(prefix + 'validate invitations', t => {
-    database_model_1.default.connect();
-    // t.is(result.value, obj);
-});
--- identity-controller/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const path_1 = require("path");
-const admin_1 = require("./app/admin/admin");
-dotenv_1.config({ path: path_1.resolve(__dirname, '../config.env') });
-admin_1.default.listen(process.env.PORT, () => {
-    console.log('listening on port: ', process.env.port);
-});

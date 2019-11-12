b649a2a2f4194d2f5bf738e456718468dc182267
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-12 11:58:50 -0800

    Add keycloak integration to wa-admin app
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- wa-admin/src/app/app.module.ts
+++ wa-admin/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicModule } from "@ionic/angular";
+import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { SharedModule } from "./shared/shared/shared.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { IonicModule } from '@ionic/angular';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared/shared.module';
+const keycloakService = new KeycloakService();
 
 @NgModule({
   declarations: [AppComponent, LoginFormComponent, NotFoundComponent],
@@ -15,13 +17,43 @@ import { SharedModule } from './shared/shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     IonicModule.forRoot({
-      mode: 'ios',
+      mode: "ios",
       animated: true
     }),
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: KeycloakService,
+      useValue: keycloakService
+    }
+  ],
+  entryComponents: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  ngDoBootstrap(app) {
+    keycloakService
+      .init({
+        config: {
+          url: "http://localhost:8180/auth",
+          realm: "identity-kit",
+          clientId: "identity-kit-admin"
+        },
+        initOptions: {
+          onLoad: "check-sso",
+          checkLoginIframe: false
+        },
+        bearerExcludedUrls: ["/assets"]
+      })
+      .then(() => {
+        console.log("[ngDoBootstrap] bootstrap app");
+
+        app.bootstrap(AppComponent);
+      })
+      .catch(error =>
+        console.error("[ngDoBootstrap] init Keycloak failed", error)
+      );
+  }
+}

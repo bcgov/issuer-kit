d88d5c12fc44eeffaefe16cedfaa97f1c32c1721
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-12 14:59:47 -0800

    Extract keycloak app config to JSON file
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- wa-admin/src/app/app.module.ts
+++ wa-admin/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { IonicModule } from "@ionic/angular";
+import Axios from "axios";
 import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -34,26 +35,31 @@ const keycloakService = new KeycloakService();
 })
 export class AppModule {
   ngDoBootstrap(app) {
-    keycloakService
-      .init({
-        config: {
-          url: "http://localhost:8180/auth",
-          realm: "identity-kit",
-          clientId: "identity-kit-admin"
-        },
-        initOptions: {
-          onLoad: "check-sso",
-          checkLoginIframe: false
-        },
-        bearerExcludedUrls: ["/assets"]
-      })
-      .then(() => {
-        console.log("[ngDoBootstrap] bootstrap app");
+    // Import app configuration from JSON
+    Axios.get("/assets/data/appConfig.json").then(response => {
+      console.log(`Loaded configuration from ${response.config.url}`);
+      const config = response.data;
+      keycloakService
+        .init({
+          config: {
+            url: config.keycloak.url,
+            realm: config.keycloak.realm,
+            clientId: config.keycloak.clientId
+          },
+          initOptions: {
+            onLoad: "check-sso",
+            checkLoginIframe: false
+          },
+          bearerExcludedUrls: ["/assets"]
+        })
+        .then(() => {
+          console.log("[ngDoBootstrap] bootstrap app");
 
-        app.bootstrap(AppComponent);
-      })
-      .catch(error =>
-        console.error("[ngDoBootstrap] init Keycloak failed", error)
-      );
+          app.bootstrap(AppComponent);
+        })
+        .catch(error =>
+          console.error("[ngDoBootstrap] init Keycloak failed", error)
+        );
+    });
   }
 }

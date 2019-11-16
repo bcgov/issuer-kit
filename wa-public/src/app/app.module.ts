964924fd859b9764c9e9f096844dca491d9b50fd
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-15 16:32:38 -0800

    Initial guard setup for public webapp
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- wa-public/src/app/app.module.ts
+++ wa-public/src/app/app.module.ts
@@ -1,32 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import Axios from 'axios';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { SuccessComponent } from './pages/success/success.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
+import { SuccessComponent } from './pages/success/success.component';
 
 const keycloakService = new KeycloakService();
 
 @NgModule({
-  declarations: [AppComponent,
+  declarations: [
+    AppComponent,
     HomeComponent,
     SuccessComponent,
-    PageNotFoundComponent],
-  imports: [BrowserModule,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    KeycloakAngularModule,
     MatSidenavModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule, KeycloakAngularModule],
+    MatIconModule
+  ],
   providers: [
     {
       provide: KeycloakService,

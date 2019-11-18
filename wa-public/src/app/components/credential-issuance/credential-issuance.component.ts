3ad635f5e9bafb8bd169f499171ef7fc02ad3178
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-18 10:07:48 -0800

    Add credential-issuance component
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

---
+++ wa-public/src/app/components/credential-issuance/credential-issuance.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wap-credential-issuance',
+  template: `
+    <p>
+      credential-issuance works!
+    </p>
+  `,
+  styleUrls: ['./credential-issuance.component.scss']
+})
+export class CredentialIssuanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

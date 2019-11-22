a5c38808bf8087a7cc3923be3c4d37d353a53cbb
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-22 15:40:06 -0800

    Add deployment config for db, wallet.
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/identity-kit-db/identity-kit-db-deploy.overrides.sh
+++ openshift/templates/identity-kit-db/identity-kit-db-deploy.overrides.sh
@@ -15,15 +15,15 @@ fi
 if createOperation; then
   # Randomly generate a set of credentials without asking ...
   printStatusMsg "Creating a set of random user credentials ..."
-  writeParameter "POSTGRESQL_USER" $(generateUsername) "false"
-  writeParameter "POSTGRESQL_PASSWORD" $(generatePassword) "false"
-  writeParameter "POSTGRESQL_ADMIN_PASSWORD" $(generatePassword) "false"
+  writeParameter "MONGODB_USER" $(generateUsername) "false"
+  writeParameter "MONGODB_PASSWORD" $(generatePassword) "false"
+  writeParameter "MONGODB_ADMIN_PASSWORD" $(generatePassword) "false"
 else
   # Secrets are removed from the configurations during update operations ...
   printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials ...\n"
-  writeParameter "POSTGRESQL_USER" "generation_skipped" "false"
-  writeParameter "POSTGRESQL_PASSWORD" "generation_skipped" "false"
-  writeParameter "POSTGRESQL_ADMIN_PASSWORD" "generation_skipped" "false"
+  writeParameter "MONGODB_USER" "generation_skipped" "false"
+  writeParameter "MONGODB_PASSWORD" "generation_skipped" "false"
+  writeParameter "MONGODB_ADMIN_PASSWORD" "generation_skipped" "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"

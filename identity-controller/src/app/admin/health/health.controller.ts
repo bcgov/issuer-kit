d07a5d4b5649f01ce5467e54067646e3cc64dfd8
Sean Hamilton <hamiltonsean6@icloud.com>
2019-11-22 16:44:21 -0800

    added a health check route
    Signed-off-by: Sean Hamilton <hamiltonsean6@icloud.com>

---
+++ identity-controller/src/app/admin/health/health.controller.ts
@@ -0,0 +1,10 @@
+import { Context } from 'koa';
+import * as Router from 'koa-router';
+
+const routerOpts = { prefix: '/health' };
+
+const router = new Router(routerOpts);
+
+router.get('/', async (ctx: Context) => {
+  return (ctx.status = 200);
+});

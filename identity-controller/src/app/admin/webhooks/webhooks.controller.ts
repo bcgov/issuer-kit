a5b1aa11c71db6dcadefe1f28007cd64425c41a1
Sean Hamilton <hamiltonsean6@icloud.com>
2019-11-27 15:44:58 -0800

    webhooks are now collecting through the agent controller
    Signed-off-by: Sean Hamilton <hamiltonsean6@icloud.com>

--- identity-controller/src/app/admin/webhooks/webhooks.controller.ts
+++ identity-controller/src/app/admin/webhooks/webhooks.controller.ts
@@ -1,6 +1,23 @@
 import * as Router from 'koa-router';
 import { Context } from 'koa';
 
+export interface IConnectionActivity {
+  created_at: string;
+  their_did: string;
+  invitation_key: string;
+  state: string;
+  invitation_mode: string;
+  accept: string;
+  connection_id: string;
+  my_did: string;
+  initiator: string;
+  request_id: string;
+  updated_at: string;
+  routing_state: string;
+  their_label: string;
+}
+
+
 const routerOpts = {
   prefix: '/topic'
 };
@@ -8,12 +25,15 @@ const routerOpts = {
 const router = new Router(routerOpts);
 
 router.post('/connections', async (ctx: Context) => {
-  console.log(ctx.request.body);
+  const data = ctx.request.body as IConnectionActivity;
   return ctx.status = 200
 });
 
 router.post('/issue_credential', async (ctx: Context) => {
-  console.log(ctx.request.body);
+  const data = ctx.request.body;
+  console.log(JSON.stringify(data, null, 2))
+
+  // if (data.)
   return ctx.status = 200
 
 });

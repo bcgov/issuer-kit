1dd445e13219127f29d22b65dd25fb8e79d9eb9c
seanadipose <hamiltonsean6@icloud.com>
2019-12-11 18:30:27 +0000

    updated verbiage on the completed page
    Signed-off-by: seanadipose <hamiltonsean6@icloud.com>

--- wa-public/src/app/pages/completed/completed.component.ts
+++ wa-public/src/app/pages/completed/completed.component.ts
@@ -5,38 +5,41 @@ import { StateService } from 'src/app/services/state.service';
 @Component({
   selector: 'wap-completed',
   template: `
-  <ion-header *ngIf="$title | async as title">
-  <ion-toolbar color="primary">
-    <ion-title> {{ title }}</ion-title>
-
-  </ion-toolbar>
-</ion-header>
-<wap-view-wrapper>
-  <mat-card>
-    <mat-card-header>
-    <img mat-card-avatar src="assets/VON-Logo.png" alt="VON Network logo" class="header-image" />
-      <mat-card-title>
-        Congratulations on issuing a credential!
-      </mat-card-title>
-      <mat-card-subtitle>
-        We have logged you out of the demo and marked it as completed. 
-        To issue yourself another credential please go <a [href]="pocLink">here.</a>
-      </mat-card-subtitle>
-    </mat-card-header>
-  </mat-card>
-</wap-view-wrapper>
+    <ion-header *ngIf="$title | async as title">
+      <ion-toolbar color="primary">
+        <ion-title> {{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <wap-view-wrapper>
+      <mat-card>
+        <mat-card-header>
+          <img mat-card-avatar src="assets/VON-Logo.png" alt="VON Network logo" class="header-image" />
+          <mat-card-title>
+            Congratulations on issuing a credential!
+          </mat-card-title>
+          <mat-card-subtitle>
+            You have completed the Identity Kit Proof of Concept(POC).
+          </mat-card-subtitle>
+        </mat-card-header>
+        <mat-card-content>
+          <br />
+          <p></p>
+          <p>
+            To try out your new verified person credential please go to this <a [href]="pocLink">demo site</a>
+          </p></mat-card-content
+        >
+      </mat-card>
+    </wap-view-wrapper>
   `,
-  styleUrls: ['./completed.component.scss']
+  styleUrls: ['./completed.component.scss'],
 })
 export class CompletedComponent implements OnInit {
-  $title = of('Identity Kit POC')
+  $title = of('Identity Kit POC');
   pocLink: string;
 
-  constructor(private stateSvc: StateService) { 
-    this.pocLink = `validate?invite_token=${this.stateSvc.linkId}`
+  constructor(private stateSvc: StateService) {
+    this.pocLink = `https://law-society-demo.pathfinder.gov.bc.ca/`;
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

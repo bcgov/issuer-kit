a7ffeb7cb75732a89f65892f4bcbefa910fe785a
seanadipose <hamiltonsean6@icloud.com>
2019-12-19 19:53:00 +0000

    finished typeahead changes
    Signed-off-by: seanadipose <hamiltonsean6@icloud.com>

--- wa-public/src/app/services/typeahead.service.ts
+++ wa-public/src/app/services/typeahead.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, from } from 'rxjs';
 import { CpRequest } from '../models/cp-request';
+import { map } from 'rxjs/operators';
 
 export interface ICPItem {
   Error?: number;
@@ -18,23 +19,28 @@ export interface ICPItem {
   providedIn: 'root',
 })
 export class TypeaheadService {
-  $address: Observable<{ items: ICPItem[] }>;
+  $addresses: Observable<ICPItem[]>;
   path = 'https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/AutoComplete/v1.00/json3.ws';
 
   constructor(private http: HttpClient) {}
 
   set address(val: any) {
-    this.$address = of(val);
+    this.$addresses = of(val);
   }
 
   queryCp(req: any) {
-    // const params = req as { [param: string]: string };
-    const queryParams = '?' + Object.keys(req).map(key => `${key}=${req[key]}`).join('&')
-    const url = `${this.path}${queryParams}`
-    console.log('req url', url)
-    const $res = this.http.get<{ items: ICPItem[] }>(url);
-    $res.subscribe(obs => console.log(obs));
-    return (this.$address = from($res));
+    const url = () => {
+      const queryParams =
+        '?' +
+        Object.keys(req)
+          .map(key => `${key}=${req[key]}`)
+          .join('&');
+      return () => `${this.path}${queryParams}`;
+    };
+
+    console.log('req url', url()());
+    const $res = this.http.get<{ Items: ICPItem[] }>(url()());
+    return (this.$addresses = $res.pipe(map(obs => obs.Items.filter(itm => itm.Text !== req))));
     // https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/AutoComplete/v1.00/json3.ws
   }
 }

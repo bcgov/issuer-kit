d4a45bbfb23dc3004a44746c7559b04cf5f572f6
Emiliano Suñé <emiliano.sune@gmail.com>
2020-01-28 09:49:38 -0800

    Parametrize config map name in override scripts
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/identity-kit-admin/identity-kit-admin-deploy.overrides.sh
+++ openshift/templates/identity-kit-admin/identity-kit-admin-deploy.overrides.sh
@@ -5,17 +5,16 @@ else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
 fi
 
-
 OUTPUT_FORMAT=json
+read -r -p $"Please provide the name for the config map holding the Caddy configuration (it must match the name used in the template):" CADDY_CONFIG_MAP_NAME
+read -r -p $"Please provide the name for the config map holding the webapp configuration (it must match the name used in the template):" APPCONFIG_CONFIG_MAP_NAME
 
 # The generated config maps are used to replace Caddyfile and config.json
-CADDY_CONFIG_MAP_NAME=identity-kit-admin-caddy-conf
 CADDY_SOURCE_FILE=$( dirname "$0" )/../../../docker/wa-admin/config/Caddyfile
 CADDY_OUTPUT_FILE=${CADDY_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
 printStatusMsg "Generating ConfigMap; ${CADDY_CONFIG_MAP_NAME} ..."
 generateConfigMap "${CADDY_CONFIG_MAP_NAME}" "${CADDY_SOURCE_FILE}" "${OUTPUT_FORMAT}" "${CADDY_OUTPUT_FILE}"
 
-APPCONFIG_CONFIG_MAP_NAME=identity-kit-admin-appconfig
 APPCONFIG_SOURCE_FILE=$( dirname "$0" )/../../../wa-admin/src/assets/config/config.json
 APPCONFIG_OUTPUT_FILE=${APPCONFIG_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
 printStatusMsg "Generating ConfigMap; ${APPCONFIG_CONFIG_MAP_NAME} ..."

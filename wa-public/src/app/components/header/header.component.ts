1717ee9d27ec42b5aa5f2fcd6e86bd6d70e3caaa
Emiliano Suñé <emiliano.sune@gmail.com>
2020-02-20 10:16:21 -0800

    Move header html template from separate file to component file
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- wa-public/src/app/components/header/header.component.ts
+++ wa-public/src/app/components/header/header.component.ts
@@ -3,7 +3,25 @@ import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'wap-header',
-  templateUrl: './header.component.html',
+  template: `
+    <ion-header>
+      <ion-toolbar color="primary">
+
+        <ion-title>
+          <img class="branding-logo" src="/assets/bcgov/bcid-logo-rev-en.svg" width="181" height="44" alt="B.C. Government Logo">
+          <span>{{ title }}</span>
+        </ion-title>
+
+        <ion-buttons slot="primary" *ngIf="isLoggedIn">
+          <ion-button (click)="logout()">
+            <ion-label>Log Out</ion-label>
+            &nbsp;
+            <ion-icon name="log-out"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+  `,
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {

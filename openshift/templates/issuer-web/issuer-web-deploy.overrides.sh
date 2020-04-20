7f4486184f267f0c664213a7c783d2bc5517152c
Emiliano Suñé <emiliano.sune@gmail.com>
2020-04-20 11:18:01 -0700

    Update issuer-web deployment template and override script
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/issuer-web/issuer-web-deploy.overrides.sh
+++ openshift/templates/issuer-web/issuer-web-deploy.overrides.sh
@@ -7,16 +7,38 @@ fi
 
 OUTPUT_FORMAT=json
 
-# The generated config maps are used to replace Caddyfile and config.json
-CADDY_SOURCE_FILE=$( dirname "$0" )/../../../docker/issuer-web/config/Caddyfile
+# Generate Caddyfile
+# Injected by genDepls.sh
+# - CADDY_CONFIG_MAP_NAME
+# - SUFFIX
+CADDY_SOURCE_FILE=$( dirname "$0" )/config/Caddyfile
 CADDY_OUTPUT_FILE=${CADDY_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
 printStatusMsg "Generating ConfigMap; ${CADDY_CONFIG_MAP_NAME} ..."
 generateConfigMap "${CADDY_CONFIG_MAP_NAME}${SUFFIX}" "${CADDY_SOURCE_FILE}" "${OUTPUT_FORMAT}" "${CADDY_OUTPUT_FILE}"
 
-APPCONFIG_SOURCE_FILE=$( dirname "$0" )/../../../docker/issuer-web/config/config.json
-APPCONFIG_OUTPUT_FILE=${APPCONFIG_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
-printStatusMsg "Generating ConfigMap; ${APPCONFIG_CONFIG_MAP_NAME} ..."
-generateConfigMap "${APPCONFIG_CONFIG_MAP_NAME}${SUFFIX}" "${APPCONFIG_SOURCE_FILE}" "${OUTPUT_FORMAT}" "${APPCONFIG_OUTPUT_FILE}"
+# Generate application config map
+# - To include all of the files in the application instance's profile directory.
+# Injected by genDepls.sh
+# - APP_CONFIG_MAP_NAME
+# - SUFFIX
+# - DEPLOYMENT_ENV_NAME
+
+# Combine the profile's default config files with its environment specific config files before generating the config map ...
+profileRoot=$( dirname "$0" )/config/${PROFILE}
+profileEnv=${profileRoot}/${DEPLOYMENT_ENV_NAME}
+profileTmp=$( dirname "$0" )/config/${PROFILE}/tmp
+mkdir -p ${profileTmp}
+cp -f ${profileRoot}/* ${profileTmp} 2>/dev/null
+cp -f ${profileEnv}/* ${profileTmp} 2>/dev/null
+
+# Generate the config map ...
+APPCONFIG_SOURCE_PATH=${profileTmp}
+APPCONFIG_OUTPUT_FILE=${APP_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+printStatusMsg "Generating ConfigMap; ${APP_CONFIG_MAP_NAME} ..."
+generateConfigMap "${APP_CONFIG_MAP_NAME}${SUFFIX}" "${APPCONFIG_SOURCE_PATH}" "${OUTPUT_FORMAT}" "${APPCONFIG_OUTPUT_FILE}"
+
+# Remove temporary configuration directory and files ....
+rm -rf ${profileTmp}
 
 unset SPECIALDEPLOYPARMS
 echo ${SPECIALDEPLOYPARMS}

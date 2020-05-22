c1ced877cbfcdd874636724a2c402bff26f69fa7
Emiliano Suñé <emiliano.sune@gmail.com>
2020-05-22 14:21:47 -0700

    Remove unused mailer service hook definitions
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- api/src/services/mailer/mailer.hooks.ts
+++ api/src/services/mailer/mailer.hooks.ts
@@ -3,31 +3,13 @@ import { disallow } from "feathers-hooks-common";
 export default {
   before: {
     all: [disallow("external")],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
   },
 
   after: {
     all: [disallow("external")],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
   },
 
   error: {
     all: [disallow("external")],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
   },
 };

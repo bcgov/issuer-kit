aa6bc44d637fa94d78214d862d1dd5776799f13b
Emiliano Suñé <emiliano.sune@gmail.com>
2020-06-09 09:53:27 -0700

    Updated openshift configurations
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,11 @@
 export PROJECT_NAMESPACE="devex-von"
 export GIT_URI="https://github.com/bcgov/identity-kit-poc.git"
 export GIT_REF="master"
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Override the DEV, PROD deployment environment
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export DEV="test"
+export TEST="test"
+export PROD="prod"

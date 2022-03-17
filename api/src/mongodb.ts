371f4ee37eb04165c24deabdc72435cb17cb134f
wadeking98 <wkingnumber2@gmail.com>
2022-03-17 12:08:21 -0700

    added healthcheck to api
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- api/src/mongodb.ts
+++ api/src/mongodb.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import { Application } from "./declarations";
 
+export let db_status = 503;
+
 export default function (app: Application) {
   const settings = app.get("mongodb");
   const connection = `mongodb://${settings.user}:${settings.password}@${settings.host}:${settings.port}/${settings.db}`;
@@ -13,7 +15,10 @@ export default function (app: Application) {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: serverSelectionTimeout*1000
-  }).then((client) => client.db(database))
+  }).then((client) => {
+    db_status = 200;
+    return client.db(database);
+  })
   .catch((error) =>{
     console.error("Database connection failed", error);
     process.exit(1);

c3b0ae96af086fce0cbbc8c79d378f542ec234b3
Ian Costanzo <ian@anon-solutions.ca>
2022-04-26 09:04:57 -0700

    Fix traction webhook handling
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- api/src/services/webhooks/webhooks.class.ts
+++ api/src/services/webhooks/webhooks.class.ts
@@ -19,7 +19,10 @@ interface Data {
   revoc_reg_id?: string;
 }
 
-interface TractionData {}
+interface TractionData {
+  topic?: string;
+  payload: Data;
+}
 
 interface ServiceOptions {}
 
@@ -101,6 +104,59 @@ export class TractionWebhooks {
 
   async create(data: TractionData, params?: Params): Promise<any> {
     logger.warn(`Webhook ${data} received`)
-    return { result: "Success" };
+    const topic = data.topic;
+    switch (topic) {
+      case WebhookTopic.Connections:
+        this.handleConnection(data.payload);
+        return { result: "Success" };
+      case WebhookTopic.IssueCredential:
+        this.handleIssueCredential(data.payload as Data);
+        return { result: "Success" };
+      default:
+        logger.warn(`Webhook ${topic} is not supported`)
+        //return new NotImplemented(`Webhook ${topic} is not supported`);
+        return { result: "Success" };
+    }
+  }
+
+  private async handleConnection(data: Data): Promise<any> {
+    // implement your connection webhook logic here
+  }
+
+  private async handleIssueCredential(data: Data): Promise<any> {
+    switch (data.state) {
+      case CredExState.RequestReceived:
+        const attributes = data.credential_proposal_dict?.credential_proposal
+          ?.attributes as AriesCredentialAttribute[];
+        await this.app.service("aries-agent").create({
+          service: ServiceType.CredEx,
+          action: ServiceAction.Issue,
+          data: {
+            credential_exchange_id: data.credential_exchange_id,
+            attributes: attributes,
+          },
+        });
+        return { result: "Success" };
+      case CredExState.Issued:
+        console.log(
+          `Credential issued for cred_ex_id ${data.credential_exchange_id}`
+        );
+        updateInviteRecord(
+          { credential_exchange_id: data.credential_exchange_id },
+          {
+            issued: true,
+            revoked: data.revocation_id ? false : undefined,
+            revocation_id: data.revocation_id,
+            revoc_reg_id: data.revoc_reg_id,
+          },
+          this.app
+        );
+        return { result: "Success" };
+      default:
+        console.warn(
+          `Received unexpected state ${data.state} for cred_ex_id ${data.credential_exchange_id}`
+        );
+        return { status: `Unexpected state ${data.state}` };
+    }
   }
 }
